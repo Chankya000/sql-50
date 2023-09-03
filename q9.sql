3b7ba82a78f1b25996aac3cd72fd9014ad40719e
Chanakya000 <sgrsharma720@gmail.com>
2023-09-03 03:23:23 -0400

    day 3

--- q9.sql
+++ q9.sql
@@ -1,6 +1,6 @@
 --Find all dates' Id with higher temperatures compared to its previous dates (yesterday).
 
-Return the result table in any order.
+--Return the result table in any order.
 
 /* Write your T-SQL query statement below */
 select wc.id as Id from weather wc , weather wp  where wc.temperature > wp.temperature and wc.recordDate = DATEADD(day,1,wp.recordDate);
